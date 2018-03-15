047092a829aa3e6389b08b71d33734c17dd526eb
Enes Kaya <eneskaya@me.com>
2018-03-15 21:43:01 +0100

    Adds Tailwind CSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,12 @@
 /* eslint-disable import/no-extraneous-dependencies */
 // PostCSS-Loader config options
+var tailwind = require('tailwindcss');
+
 module.exports = {
-  plugins: [require('postcss-simple-vars'), require('postcss-cssnext'), require('postcss-import')],
+  plugins: [
+    require('postcss-simple-vars'),
+    require('postcss-cssnext'),
+    require('postcss-import'),
+    tailwind('./tailwind.js'),
+  ],
 };
